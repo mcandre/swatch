751a47deaf83fbb6254505d56ece4b01479abdc9
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-08-04 14:27:57 -0500

    pep8

--- python/swatch.py
+++ python/swatch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Swatch time / Internet time"""
+'''Swatch time / Internet time'''
 
 # Andrew Pennebaker
 # Copyright 2007 Andrew Pennebaker
@@ -10,26 +10,28 @@
 
 import time
 
+
 def beats():
-  """Swatch beats"""
+    '''Swatch beats'''
 
-  t = time.gmtime()
-  h, m, s = t.tm_hour, t.tm_min, t.tm_sec
+    t = time.gmtime()
+    h, m, s = t.tm_hour, t.tm_min, t.tm_sec
 
-  utc = 3600 * h + 60 * m + s # UTC
+    utc = 3600 * h + 60 * m + s  # UTC
 
-  bmt = utc + 3600 # Biel Mean Time (BMT)
+    bmt = utc + 3600  # Biel Mean Time (BMT)
 
-  beat = bmt / 86.4
+    beat = bmt / 86.4
 
-  if beat > 1000:
-    beat -= 1000
+    if beat > 1000:
+        beat -= 1000
 
-  return beat
+    return beat
+
 
 def swatch():
-  """Swatch time"""
-  return "@%06.2f" % (beats())
+    '''Swatch time'''
+    return '@%06.2f' % (beats())
 
-if __name__ == "__main__":
-  print(swatch())
+if __name__ == '__main__':
+    print(swatch())
